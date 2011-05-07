afd9f18ad6c90accf5b1c20cf0846808fd7fc2e9
Grégory BELLIER <gregory.bellier@gmail.com>
2011-05-07 20:44:30 +0200

    Exception caught in test.mt

--- test.ml
+++ test.ml
@@ -4,6 +4,7 @@
 open Fdinfo
 
 let _ =
+  
   let p = pid_of_int 3634 in
   
   let l =
@@ -11,11 +12,17 @@ let _ =
       get_fds p
     with Unix.Unix_error (e,_,_) ->
       prerr_endline (Unix.error_message e) ;
-      assert false
+      []
   in
   
-  let fd = List.find (fun fd -> fd.num = 27) l in
-  
-  Printf.printf "File %s. Offset: %d\n" fd.name (Int64.to_int (get_offset p fd));
-  Pervasives.flush Pervasives.stdout
+  try
+    (* The test can also be performed on the filename *)
+    let fd = List.find (fun fd -> fd.num = 27) l in    
+    
+    Printf.printf "File %s. Offset: %d\n" fd.name (Int64.to_int (get_offset p fd));
+    Pervasives.flush Pervasives.stdout
+      
+  with Not_found ->
+    prerr_endline "No such file descriptor for this pid"
+      
 ;;
