d69db0b1b3c93d3d384f13dd2da5a16dc34f5ede
Grégory BELLIER <gregory.bellier@gmail.com>
2011-05-26 18:19:14 +0200

    Fix for ocamldoc

--- fdinfo.mli
+++ fdinfo.mli
@@ -1,3 +1,9 @@
+(**
+
+	This module provides informations on file descriptors opened by another processus
+
+*)
+
 exception Fdinfo_parse_error
 
 type fdinfo = {
@@ -11,5 +17,5 @@ type pid
     
 val pid_of_int : int -> pid
 
-(** Returns all the fds opened by a pid. In case of errors, it can raise both the Unix module's exceptions and Fdinfo_parse_error **)
+(** [get pid] returns file descriptors informations [fdinfo list] opened by [pid]. In case of errors, it can raise both the Unix module's exceptions and [Fdinfo_parse_error] *)
 val get : pid -> fdinfo list
